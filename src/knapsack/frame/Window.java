5986fd8604ed74283fdbcccb00deb1c3473f5253
royceda <caucricity@gmail.com>
2015-10-08 16:06:24 +0200

    swing

--- src/knapsack/frame/Window.java
+++ src/knapsack/frame/Window.java
@@ -13,6 +13,8 @@ import javax.swing.JButton;
 import java.awt.Insets;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 
 public class Window extends JFrame {
 
@@ -48,48 +50,79 @@ public class Window extends JFrame {
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
+		JLabel lblValues = new JLabel("Values");
+		GridBagConstraints gbc_lblValues = new GridBagConstraints();
+		gbc_lblValues.insets = new Insets(0, 0, 5, 5);
+		gbc_lblValues.gridx = 0;
+		gbc_lblValues.gridy = 2;
+		contentPane.add(lblValues, gbc_lblValues);
+		
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 9;
+		gbc_textField.gridx = 1;
 		gbc_textField.gridy = 2;
 		contentPane.add(textField, gbc_textField);
 		textField.setColumns(10);
 		
+		JLabel lblWeight = new JLabel("Weight");
+		GridBagConstraints gbc_lblWeight = new GridBagConstraints();
+		gbc_lblWeight.insets = new Insets(0, 0, 5, 5);
+		gbc_lblWeight.gridx = 0;
+		gbc_lblWeight.gridy = 3;
+		contentPane.add(lblWeight, gbc_lblWeight);
+		
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 9;
+		gbc_textField_1.gridx = 1;
 		gbc_textField_1.gridy = 3;
 		contentPane.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 		
 		JComboBox comboBox = new JComboBox();
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 9;
-		gbc_comboBox.gridy = 10;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 5;
 		contentPane.add(comboBox, gbc_comboBox);
 		
 		JButton btnCompute = new JButton("Compute");
 		GridBagConstraints gbc_btnCompute = new GridBagConstraints();
-		gbc_btnCompute.insets = new Insets(0, 0, 0, 5);
-		gbc_btnCompute.gridx = 8;
-		gbc_btnCompute.gridy = 13;
+		gbc_btnCompute.insets = new Insets(0, 0, 5, 5);
+		gbc_btnCompute.gridx = 0;
+		gbc_btnCompute.gridy = 7;
 		contentPane.add(btnCompute, gbc_btnCompute);
 		
 		JButton btnCancel = new JButton("Cancel");
 		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
-		gbc_btnCancel.gridx = 9;
-		gbc_btnCancel.gridy = 13;
+		gbc_btnCancel.insets = new Insets(0, 0, 5, 5);
+		gbc_btnCancel.gridx = 1;
+		gbc_btnCancel.gridy = 7;
 		contentPane.add(btnCancel, gbc_btnCancel);
+		
+		JLabel lblSolution = new JLabel("Solution:");
+		GridBagConstraints gbc_lblSolution = new GridBagConstraints();
+		gbc_lblSolution.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSolution.gridx = 0;
+		gbc_lblSolution.gridy = 9;
+		contentPane.add(lblSolution, gbc_lblSolution);
+		
+		JProgressBar progressBar = new JProgressBar();
+		GridBagConstraints gbc_progressBar = new GridBagConstraints();
+		gbc_progressBar.anchor = GridBagConstraints.WEST;
+		gbc_progressBar.gridwidth = 2;
+		gbc_progressBar.insets = new Insets(0, 0, 5, 5);
+		gbc_progressBar.gridx = 0;
+		gbc_progressBar.gridy = 12;
+		contentPane.add(progressBar, gbc_progressBar);
 	}
 
 }
